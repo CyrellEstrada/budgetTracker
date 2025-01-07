53dca4acce917c5da6cf55d9e7599cd514692627
CyrellEstrada <cyrellestrada1@gmail.com>
2025-01-06 19:13:51 -0600

    Notes added to file to keep in mind.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -16,6 +16,9 @@ const login = (req, res) => {
       grant_type: 'client_credentials',
       username: username,
       password: password,
+      // •	Just know that the client_credentials flow is typically for machine-to-machine tokens, not for a typical user “login form.”
+      // •	If your goal is a user login, you might need the Resource Owner Password flow or the Auth0 Hosted Login Page approach.
+      // •	However, if this is just a test or a special use case, it might be fine.
       audience: process.env.AUTH0_IDENTITY,
       // connection: 'Username-Password-Authentication',
       client_id: process.env.AUTH0_CLIENT_ID,
