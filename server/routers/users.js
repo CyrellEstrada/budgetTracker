0e8e74cc9bcc1617c618650721773fec1b0adc5a
CyrellEstrada <cyrellestrada1@gmail.com>
2025-01-06 19:12:59 -0600

    Corrected function names on multiple routes. Code is working as intended. NOTE: "checkJwt" was taken off routes for practicallity when testing, insure "checkJwt" is on all routes when this goes to production.

--- server/routers/users.js
+++ server/routers/users.js
@@ -5,12 +5,12 @@ const router = express.Router()
 
 router.get('/', usersController.getAllUsers)
 
-router.get('/:id', usersController.getUserById)
+router.get('/:id', usersController.getUserByUserId)
 
-router.post('/', checkJwt, usersController.createUser)
+router.post('/', usersController.createUser)
 
-router.put('/:id', checkJwt, usersController.updateUserById)
+router.put('/:id', usersController.updateUserById)
 
-router.delete('/:first_name', checkJwt, usersController.deleteUserByFirstName)
+router.delete('/:first_name', usersController.deleteUserByUsername)
 
 module.exports = router
