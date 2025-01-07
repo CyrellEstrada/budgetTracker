31d2ff5b7095c546b832dac413612f636cfe10cd
CyrellEstrada <cyrellestrada1@gmail.com>
2025-01-06 20:40:54 -0600

    Imported routes from './server/routers/budget' and made '/budget' endpoint.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require('dotenv').config()
 const express = require("express");
 const usersRouter = require('./server/routers/users');
+const budgetRouter = require('./server/routers/budget')
 const authRouter = require('./server/routers/auth');
 const app = express();
 const port = process.env.PORT || 4001;
 
 app.use(express.json())
 app.use('/users', usersRouter)
+app.use('/budget', budgetRouter)
 app.use('/auth', authRouter)
 
 app.get('/', (req, res) => {
